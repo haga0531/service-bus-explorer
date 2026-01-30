3234737a4afaf4d1c5abfcb2879a8f5f6a3e5c33
Hiroki Haga <hiroki.haga@sansan.com>
2026-01-30 18:06:12 +0900

    fix: Remove duplicate CloseRequested handler in ResubmitConfirmDialog (#17)

--- src/ServiceBusExplorer.UI/ResubmitConfirmDialog.axaml.cs
+++ src/ServiceBusExplorer.UI/ResubmitConfirmDialog.axaml.cs
@@ -7,27 +7,5 @@ public partial class ResubmitConfirmDialog : Window
     public ResubmitConfirmDialog()
     {
         InitializeComponent();
-        
-        // Subscribe to view model events when DataContext is set
-        DataContextChanged += OnDataContextChanged;
-    }
-    
-    private void OnDataContextChanged(object? sender, System.EventArgs e)
-    {
-        if (DataContext is ResubmitConfirmDialogViewModel vm)
-        {
-            vm.CloseRequested += OnCloseRequested;
-        }
-    }
-    
-    private void OnCloseRequested(object? sender, (bool confirmed, bool deleteFromDeadLetter) result)
-    {
-        if (DataContext is ResubmitConfirmDialogViewModel vm)
-        {
-            vm.CloseRequested -= OnCloseRequested;
-        }
-        
-        // For bulk operations, we only care about confirmed/cancelled
-        Close(result.confirmed);
     }
 }
